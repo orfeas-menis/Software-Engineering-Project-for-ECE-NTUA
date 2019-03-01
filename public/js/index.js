dd46e9e38274f24640b4a95d35a70fb5b0331308
ManBehindTheMyth <grumoxhd@gmail.com>
2019-03-01 14:06:20 +0100

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-    console.log("we are ok!");
-    var token = localStorage.getItem("token")
-    if (token == null){
-        alert("You must be logged in to add new product")
-        $(location).attr("href", "/login");
-
-    }   
+    console.log("we are ok!");   
 });
 
 var home = true;
