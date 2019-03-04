7587280f6cd8b03658e13cd4951f6b678ed67c86
unknown <menisorfeas@gmail.com>
2019-03-04 00:26:54 +0000

    Orfeas add

--- routing/addproductRouter.js
+++ routing/addproductRouter.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const addproductController = require("../controllers/addproductController");
+const authPolicy = require("../policies/AuthPolicy")
 
-router.get("/", addproductController.addproduct);
+
+router.get("/",  addproductController.addproduct);
 
 module.exports = router;
