e3a40949d53daf6458ce30a61b5da0d6fdc34f9e
stas_afou <stas_pao13@hotmail.com>
2019-03-05 04:24:26 +0200

    t

--- public/js/account.js
+++ public/js/account.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
         alert("You must be logged in to check your account")
         $(location).attr("href", "/login");
     }
-<<<<<<< HEAD
     var $infos = $("#info-template");
     var infotemplate = $('#info-template').html();
     $.ajax({
@@ -15,67 +14,53 @@ $(document).ready(function(){
             "X-OBSERVATORY-AUTH": token
         },
         success: function(infos,status) {
-            
+            var category = $infos.category;
+            var adminbutton=""+ "<button>Change user category</button>";
+            if (category=='0'){
+                $infos.append(adminbutton);
+            }
+            $infos.delegate('.editinfo', 'click',function(){
+                var $div= $(this).closest('div');
+                $div.find('input.username').val( $div.find('span.username').html() );
+                $div.find('input.email').val( $div.find('span.email').html() );
+                $div.addClass('edit');
+            });
+        
+            $infos.delegate('.cancelinfo', 'click',function(){
+                $(this).closest('div').removeClass('edit');
+            });
+        
+            $infos.delegate('.saveinfo', 'click',function(){
+                var $div= $(this).closest('div');
+                var info = {
+                    username: $infos.find('input.username').val(),
+                    email: $infos.find('input.email').val()
+                };
+        
+                $.ajax({
+                    url:"/userManagement/whoami" + $div.attr('data-id'),
+                    method: "PUT",
+                    headers: {
+                        "X-OBSERVATORY-AUTH": token
+                    },
+                    success: function(infos,status) {
+                        $div.find('span.username').html(info.username);
+                        $div.find('span.email').html(info.email);
+                        $div.removeClass('edit');
+                    },
+                    error: function(response,status){
+        
+                    }
+                });
+            });
         },
         error: function(response,status){
+            var adminbutton=""+ "<button>Change user category</button>";
+                $infos.append(adminbutton);
             alert("Something went wrong")
             console.log(response)
         }
     });
     
-    $infos.delegate('.editinfo', 'click',function(){
-        var $div= $(this).closest('div');
-        $div.find('input.username').val( $div.find('span.username').html() );
-        $div.find('input.email').val( $div.find('span.email').html() );
-        $div.addClass('edit');
-    });
-
-    $infos.delegate('.cancelinfo', 'click',function(){
-        $(this).closest('div').removeClass('edit');
-    });
-
-    $infos.delegate('.saveinfo', 'click',function(){
-        var $div= $(this).closest('div');
-        var info = {
-            username: $infos.find('input.username').val(),
-            email: $infos.find('input.email').val()
-        };
-
-        $.ajax({
-            url:"/userManagement/whoami" + $div.attr('data-id'),
-            method: "PUT",
-            headers: {
-                "X-OBSERVATORY-AUTH": token
-            },
-            success: function(infos,status) {
-                $div.find('span.username').html(info.username);
-                $div.find('span.email').html(info.email);
-                $div.removeClass('edit');
-            },
-            error: function(response,status){
-
-            }
-        });
-    });
-=======
-    else{
-        $.ajax({
-            url:"/userManagement/whoami",
-            method: "GET",
-            headers: {
-                "X-OBSERVATORY-AUTH": token
-            },
-            success: function(response,status) {
-                console.log("All good man!")
-                console.log(response)
-    
-            },
-            error: function(response,status){
-                alert("Something went wrong")
-                console.log(response)
-            }
-        })
-    }
-        
->>>>>>> master
+   
 });
