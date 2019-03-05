bb276e447527d25e6d30adac9d207dc5be00ef1a
angelospl48 <angelospl48@gmail.com>
2019-03-05 05:16:43 +0200

    index Finito

--- public/js/index.js
+++ public/js/index.js
@@ -42,14 +42,19 @@ $("#btn").click(function(event){
   window.location.assign("/login");
 })
 
+
 var tok = localStorage.getItem("token")
+$("#bton").click(function(event){
+
+})
+
+
 if (tok==null){
 
  document.getElementById('bton').style.visibility='hidden';
  document.getElementById('btn').style.visibility='visible';
 }
 else{
-
    document.getElementById('btn').style.visibility='hidden';
    document.getElementById('bton').style.visibility='visible';
 }
